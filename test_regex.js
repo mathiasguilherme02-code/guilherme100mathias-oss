1f0fc6f277048a5729ad7da8a2e588e6a719d2ee
mathiasguilherme02-code <mathiasguilherme02@gmail.com>
2026-07-20 09:57:22 -0300

    feat: add mock product data to server
    Populate mockProdutos with predefined financial service products to support initial application state. Clean up obsolete patch utility scripts.

--- test_regex.js
+++
@@ -1,7 +0,0 @@
-const regex = /^data:([A-Za-z-+\/]+);base64,(.+)$/;
-const str1 = 'data:image/jpeg;base64,12345';
-const str2 = 'data:image/png;base64,12345';
-const str3 = 'data:image/webp;base64,12345';
-console.log("str1", str1.match(regex));
-console.log("str2", str2.match(regex));
-console.log("str3", str3.match(regex));
